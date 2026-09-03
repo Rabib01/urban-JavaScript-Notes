1487d72f3c12967dfe144fab3d07404ef123d868
Muhtasim Rabib <muhtasimrabib2@gmail.com>
2026-09-03 12:07:00 +0600

    practising sets now

--- Jonas's Data Structure Lecture/script.js
+++ Jonas's Data Structure Lecture/script.js
@@ -48,6 +48,13 @@ const restaurant = {
 
 const weekDays = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
 
+{
+  // The outer [] is tellng JS to not treat it ias a literal property name, but to evaluate it first
+  // more on this shit here : https://chatgpt.com/c/6a987b89-cc30-83e8-bf6a-35073914688b
+  // when -> Whenever you want to create an object property using a variable or exoression
+  // can  also do this =>  [`item-${x}`]: "hello"
+}
+
 const openingHours = {
   [weekDays[4]]: {
     open: 12,
@@ -78,11 +85,23 @@ for (const day of Object.keys(openingHours)) {
   openString += `${day}, `;
 }
 console.log(openString);
+console.log("");
+console.log("");
 
 // Property values
 const values = Object.values(openingHours);
 console.log(values);
+console.log("");
+console.log("");
 
 // Loop through the entire object we need entries - tbc : 5:12 of 10:09
 const entries = Object.entries(openingHours);
 console.log(entries);
+console.log("");
+console.log("");
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we are open from ${open} and close at ${close} `);
+}
+
+// Sets - Collection of unique values - cannot have duplicates
